e4417b7181ded6b86cffd84791a5381d41d8cb02
Starlight Romero <starlightromero@protonmail.com>
2021-02-22 23:02:46 -0800

    [create] compare controller

---
+++ backend/controllers/compare.js
@@ -0,0 +1,26 @@
+const Libre = require('../models/libre');
+const Proprietary = require('../models/proprietary');
+
+exports.findSoftwareByName = (req, res) => {
+  Proprietary.findOne(
+    { name: req.body.softwareName },
+  ).then(
+    (proprietarySoftware) => Libre.find(
+      { category: proprietarySoftware.category },
+    ),
+  ).then(
+    (software) => res.status(200).json({ software }),
+  ).catch((err) => {
+    throw err.message;
+  });
+};
+
+exports.findSoftwareByCategory = (req, res) => {
+  Libre.find(
+    { category: req.params.softwareCategory },
+  ).then(
+    (software) => res.status(200).json({ software }),
+  ).catch((err) => {
+    throw err.message;
+  });
+};
