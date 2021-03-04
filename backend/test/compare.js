507766251df7e8c3e42f8c5c6445102d4eebdeca
Starlight Romero <starlightromero@protonmail.com>
2021-03-04 09:48:42 -0800

    [update] compare tests

--- backend/test/compare.js
+++ backend/test/compare.js
@@ -1,94 +1,82 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../app.js');
+/* eslint-env mocha */
+const mongoose = require('mongoose')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
 
-const { assert } = chai;
+const Libre = require('../models/libre')
+const Proprietary = require('../models/proprietary')
 
-const Libre = require('../models/libre.js');
-const Proprietary = require('../models/proprietary.js');
+chai.config.includeStack = true
 
-chai.config.includeStack = true;
+const { assert } = chai
+const { expect } = chai
+const should = chai.should()
+chai.use(chaiHttp)
 
-const { expect } = chai;
-const should = chai.should();
-chai.use(chaiHttp);
+const SAMPLE_LIBRE_ID = 'aaaaaaaaaaaa'
+const SAMPLE_PROPRIETARY_ID = 'bbbbbbbbbbbb'
 
-after((done) => {
-  mongoose.models = {};
-  mongoose.modelSchemas = {};
-  mongoose.connection.close();
-  done();
-});
+describe('Compare API endpoints', function () {
+  const proprietarySoftware = new Proprietary({
+    name: 'ProprietarySample',
+    category: 'Word processor',
+    _id: SAMPLE_PROPRIETARY_ID
+  })
+  const libreSoftware = new Libre({
+    name: 'LibreSample',
+    category: 'Word processor',
+    repo: 'https://gitlab.com/libresample',
+    website: 'https://sample.tk',
+    description: 'This is a sample word processor.',
+    license: 'GPL-3.0',
+    _id: SAMPLE_LIBRE_ID
+  })
 
-const SAMPLE_LIBRE_ID = 'aaaaaaaaaaaa';
-const SAMPLE_PROPRIETARY_ID = 'bbbbbbbbbbbb';
+  beforeEach(function (done) {
+    proprietarySoftware
+      .save()
+    libreSoftware
+      .save()
+      .then(function () { done() })
+  })
 
-describe('Compare API endpoints', () => {
-  beforeEach((done) => {
-    const proprietarySoftware = new Proprietary({
-      name: 'ProprietarySample',
-      category: 'Word processor',
-      _id: SAMPLE_PROPRIETARY_ID,
-    });
-    const libreSoftware = new Libre({
-      name: 'LibreSample',
-      category: 'Word processor',
-      repo: 'https://gitlab.com/libresample',
-      website: 'https://sample.tk',
-      description: 'This is a sample word processor.',
-      license: 'GPL-3.0',
-      _id: SAMPLE_LIBRE_ID,
-    });
-    proprietarySoftware.save().then(() => {
-      libreSoftware.save();
-    }).then(
-      done(),
-    );
-  });
+  afterEach(function (done) {
+    Proprietary
+      .deleteMany({ name: ['ProprietarySample', 'ProprietaryMailSample'] })
+    Libre
+      .deleteMany({ name: ['LibreSample', 'LibreMailSample'] })
+      .then(function () { done() })
+  })
 
-  afterEach((done) => {
-    Proprietary.deleteMany(
-      { name: ['ProprietarySample', 'ProprietaryMailSample'] },
-    ).then(() => {
-      Libre.deleteMany(
-        { name: ['LibreSample', 'LibreMailSample'] },
-      );
-    }).then(() => {
-      done();
-    });
-  });
+  it('should get all libre software by category of the proprietary name', function (done) {
+    chai.request(app)
+      .get('/name/ProprietarySample')
+      .end(function (err, res) {
+        if (err) { done(err) }
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.an('array')
+        done()
+      })
+  })
 
-  it('should get software by name', (done) => {
-    chai.request(
-      app,
-    ).post(
-      '/',
-    ).send(
-      {
-        softwareName: 'ProprietarySample',
-      },
-    ).end((err, res) => {
-      if (err) { done(err); }
-      expect(res).to.have.status(200);
-      expect(res.body).to.be.an('array');
-      done();
-    });
-  });
+  it('should get all libre software by category', function (done) {
+    chai.request(app)
+      .get('/category/Word%20processor')
+      .end(function (err, res) {
+        if (err) { done(err) }
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.an('object')
+        expect(res.body.name).to.equal('ProprietarySample')
+        expect(res.body.category).to.equal('Word processor')
+        done()
+      })
+  })
 
-  // it('should get one software by category', (done) => {
-  //   chai.request(
-  //     app,
-  //   ).get(
-  //     `/${SAMPLE_OBJECT_ID}`,
-  //   ).end((err, res) => {
-  //     if (err) { done(err); }
-  //     expect(res).to.have.status(200);
-  //     expect(res.body).to.be.an('object');
-  //     expect(res.body.name).to.equal('ProprietarySample');
-  //     expect(res.body.category).to.equal('Word processor');
-  //     done();
-  //   });
-  // });
-});
+  after(function (done) {
+    mongoose.models = {}
+    mongoose.modelSchemas = {}
+    mongoose.connection.close()
+    done()
+  })
+})
