a622a91db4412a8bb7497bf6328ca8ffb8bd0adf
Starlight Romero <starlightromero@protonmail.com>
2021-03-04 09:48:29 -0800

    [update] proprietary routes with isAuth

--- backend/routes/proprietary.js
+++ backend/routes/proprietary.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const proprietaryController = require('../controllers/proprietary');
 
 const router = express.Router();
 
-router.get('/', proprietaryController.getAllSoftware);
-
-router.post('/', proprietaryController.addSoftware);
-
-router.get('/:softwareId', proprietaryController.getSoftwareById);
-
-router.patch('/:softwareId', proprietaryController.updateSoftware);
-
-router.delete('/:softwareId', proprietaryController.deleteSoftware);
+const proprietaryController = require('../controllers/proprietary');
+const isAuth = require('../middleware/isAuth');
 
 router.get('/name/:softwareName', proprietaryController.getSoftwareByName);
 
 router.get('/category/:softwareCategory', proprietaryController.getSoftwareByCategory);
+
+router.get('/:softwareId', proprietaryController.getSoftwareById);
+
+router.patch('/:softwareId', isAuth, proprietaryController.updateSoftware);
+
+router.delete('/:softwareId', isAuth, proprietaryController.deleteSoftware);
+
+router.get('/', proprietaryController.getAllSoftware);
+
+router.post('/', isAuth, proprietaryController.addSoftware);
 
 module.exports = router;
